02a08606de4b6ab981e3728d7e7cf44733075716
Caue Beloni <cbeloni@gmail.com>
2025-02-03 23:03:43 -0300

    adicionado tempalte bootstrap

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from models import MsgPayload
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
 
 app = FastAPI()
 messages_list: dict[int, MsgPayload] = {}
+
+templates = Jinja2Templates(directory="templates")
 
 
 # About page route
@@ -10,4 +16,6 @@ messages_list: dict[int, MsgPayload] = {}
 def about() -> dict[str, str]:
     return {"message": "This is the about page."}
 
-
+@app.get("/", response_class=HTMLResponse)
+async def read_home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "message": "Esta é a página inicial."})
