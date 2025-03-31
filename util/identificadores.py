6d03add91a08a65d30a5eb268a075c2d6097cf1d
Caue Beloni <cbeloni@gmail.com>
2025-03-31 20:28:53 -0300

    add emails

--- util/identificadores.py
+++ util/identificadores.py
@@ -6,18 +6,18 @@ apartamento2 = ['ana carolina']
 apartamento1 = ['everton silva', 'alexandre tomassine','alexandre toma']
 
 
-# email_mapping = {
-#     'AP1': "everton.silvasousa@gmail.com",
-#     'AP2': "anacarolina.bnl@gmail.com",
-#     'AP3': "tathigarcia@hotmail.com",
-#     'AP4': "caue.beloni@unifesp.br"
-# }
 email_mapping = {
-    'AP1': "caue.beloni@unifesp.br",
-    'AP2': "caue.beloni@unifesp.br",
-    'AP3': "caue.beloni@unifesp.br",
+    'AP1': "everton.silvasousa@gmail.com",
+    'AP2': "anacarolina.bnl@gmail.com",
+    'AP3': "tathigarcia@hotmail.com",
     'AP4': "caue.beloni@unifesp.br"
 }
+# email_mapping = {
+#     'AP1': "caue.beloni@unifesp.br",
+#     'AP2': "caue.beloni@unifesp.br",
+#     'AP3': "caue.beloni@unifesp.br",
+#     'AP4': "caue.beloni@unifesp.br"
+# }
 
 caixa_mapping = {
     'AP1': Decimal('0.0'),
