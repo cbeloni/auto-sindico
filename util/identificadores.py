44f44b92b09148fae4e97d13639f784bc1cb213e
Cauê Beloni <cbeloni@gmail.com>
2025-07-21 21:31:16 -0300

    add identificador

--- util/identificadores.py
+++ util/identificadores.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 apartamento4 = ['cauê beloni', 'caue beloni','raquel santos']
-apartamento3 = ['tathiane de almeida','filipe']
+apartamento3 = ['tathiane de almeida','filipe', 'tathy modas']
 apartamento2 = ['ana carolina']
 apartamento1 = ['everton silva', 'alexandre tomassine','alexandre toma']
 
