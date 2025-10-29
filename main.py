1aa511c63921be8da73d1a857d455fad5370053a
Cauê Beloni <cbeloni@gmail.com>
2025-10-29 11:00:21 -0300

    ajustes nomes métodos

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from starlette.requests import Request
 from dto.cobrar_request import CobrarRequest
 from dto.email import EmailRequest
 from dto.extrato_request import ExtratoApiRequest
-from dto.fechamento_requests import FechamentoDespesasRequest, FechamentoRequest, FechamentoPagamentosDate
+from dto.fechamento_requests import (
+    FechamentoDespesasRequest,
+    FechamentoRequest,
+    FechamentoPagamentosDate,
+)
 from dto.pagbank_request import MovimentosPagbankParams
 from dto.pix import PixRequest
 from dto.resumo_requests import ResumoRequest
@@ -34,70 +38,86 @@ templates = Jinja2Templates(directory="templates")
 def about() -> dict[str, str]:
     return {"message": "This is the about page."}
 
+
 @app.get("/mail")
 def mail() -> list:
     return read_emails_from_gmail()
 
+
 @app.get("/drive")
-def mail() -> list:
+def drive() -> list:
     return get_last_file_from_drive()
+
 
 @app.get("/despesas")
 def despesas() -> list:
     return despesas_ordenadas_por_id_desc()
 
+
 @app.get("/caixa")
 def caixa() -> list:
     return caixa_ordenado_por_id_desc()
 
+
 @app.get("/concialiacao")
 def concialiacao() -> list:
     return concialiacao_ordenadas_por_id_desc()
+
 
 @app.post("/extrato")
 def extrato(request: ExtratoApiRequest = None) -> dict:
     if request is None:
         request = ExtratoApiRequest()
     extrato: ExtratoAbstract = factory_extrato_service(request.provider)
-    extrato_dados =  extrato.obter_extrato(request.data_inicial, request.data_final)
+    extrato_dados = extrato.obter_extrato(request.data_inicial, request.data_final)
     if request.gravar:
         extrato.gravar_extrato(extrato_dados)
     return extrato_dados
 
+
 @app.post("/fechamento-despesas")
-def fechamento(request: FechamentoDespesasRequest = None) -> dict:
+def fechamento_despesas(request: FechamentoDespesasRequest = None) -> dict:
     if request is None:
         request = FechamentoDespesasRequest()
     return fechar_despesas(request.data_inicial, request.data_final)
 
+
 @app.post("/fechamento-pagamentos")
-def fechamento(request: FechamentoRequest = None) -> dict:
+def fechamento_pagamentos(request: FechamentoRequest = None) -> dict:
     if request is None:
         request = FechamentoPagamentosDate()
     return fechar_pagamentos(request.data_inicial, request.data_final)
+
 
 @app.get("/", response_class=HTMLResponse)
 async def read_home(request: Request):
     despesas = concialiacao_ordenadas_por_id_desc()
     totais = calcular_totais(despesas)
-    return templates.TemplateResponse("home.html", {"request": request, "despesas": despesas, **totais})
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "despesas": despesas, **totais}
+    )
+
 
 @app.post("/qrcode")
-def qrcode(request: PixRequest = PixRequest(
-    name_receiver='Cauê Beloni',
-    city_receiver='Santo André',
-    key='cbeloni@gmail.com',
-    identification='12345',
-    zipcode_receiver='09291250',
-    description='condominio mensal',
-    amount=1.0
-)) -> dict:
+def qrcode(
+    request: PixRequest = PixRequest(
+        name_receiver="Cauê Beloni",
+        city_receiver="Santo André",
+        key="cbeloni@gmail.com",
+        identification="12345",
+        zipcode_receiver="09291250",
+        description="condominio mensal",
+        amount=1.0,
+    ),
+) -> dict:
     return generate_qrcode(request)
 
+
 @app.post("/send-email")
-def send_email(request: EmailRequest) -> dict:        
+def send_email(request: EmailRequest) -> dict:
     enviar_email(request.subject, request.body, request.recipient)
     return {"message": "Email sent successfully"}
+
 
 @app.post("/send-whatsapp")
 def send_whatsapp(numero: str = "5511941503226") -> dict:
@@ -107,32 +127,32 @@ def send_whatsapp(numero: str = "5511941503226") -> dict:
         send_whatsapp_message(numero, mensagem)
     return {"message": "Mensagens enviadas com sucesso"}
 
+
 @app.post("/cobrar")
 def cobrar(request: CobrarRequest = CobrarRequest()) -> dict:
-    
     cobrar_e_enviar_email(request)
     return {"message": "Email sent successfully"}
 
+
 @app.post("/resumo")
-def resumo(request: ResumoRequest = None):    
+def resumo(request: ResumoRequest = None):
     if request is None:
         request = ResumoRequest()
     return consultar_tipo_transacao(request)
 
+
 @app.post("/movimentos-pagbank")
 def movimentos_pagbank(request: MovimentosPagbankParams):
     from integrations.pagbank import consultar_movimentos_pagbank
+
     response = consultar_movimentos_pagbank(
         data_movimento=request.data_movimento,
         page_number=request.page_number,
         page_size=request.page_size,
-        tipo_movimento=request.tipo_movimento
+        tipo_movimento=request.tipo_movimento,
     )
-    
+
     if response is None:
         return {"error": "Erro ao consultar movimentos PagBank"}
-    
-    return {
-        "status_code": response.status_code,
-        "data": response.json()
-    }
+
+    return {"status_code": response.status_code, "data": response.json()}
